293168372a212375c6daba4b38c5e9667cd95553
AldenorNeto <74365018+AldenorNeto@users.noreply.github.com>
2021-10-24 20:03:46 -0300

    aprimorando funçao ajax e excluindo algumas variaveis globais

--- scriptInt2.js
+++ scriptInt2.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     
-    var fruta = ['4', '2', '2', '7', '9', '0', '6', '2', '5', '8']
+    var fruta = ['3', '2', '2', '7', '9', '0', '6', '2', '5', '8']
     var entradaAnalogica = [55,77,99,33,11]
     const ajaxValues = link => {
         
@@ -10,36 +10,22 @@ setTimeout(() => {
             arr = arr[0].split(' ');          //['xxxxxxxxxxx', 'xxxxxxxx', 'xxxxxxxxxx', 'xxxxx']
                 
 
-                fruta[parseInt(Math.random()*10)] = parseInt(Math.random()*10) + ''
-                arr = [fruta.join(''),fruta.join(''),fruta.join(''),fruta.join('')]
+                fruta[parseInt((Math.random()*9)+1)] = parseInt(Math.random()*5) + ''
+                console.log(fruta);
+                arr = [fruta.join(''),fruta.join(''),fruta.join(''),'9226109']
 
 
-
-                
             var quantidadeUDInt = arr.length        //4 ou 6
             var arrayArmazenaEmBool = []
             for(let n = 0; n < quantidadeUDInt; n++){
                 arrayArmazenaEmBool.push(parseInt(arr[n]).toString(2))        //['1010100110101', '110100010', '101011110010', '10110']
-                arrayArmazenaEmBool[n] = Array.from(arrayArmazenaEmBool[n],Math.abs).reverse().concat(Array(32 - arrayArmazenaEmBool[n].length).fill(0))
+                arrayArmazenaEmBool[n] = arrayArmazenaEmBool[n].padStart(32,0) //padroniza tamanho do array
+                arrayArmazenaEmBool[n] = Array.from(arrayArmazenaEmBool[n],Math.abs).reverse()
             }
 
-
-            var i = 0
-            for(let n = 0; n < quantidadeUDInt; n++){
-                for (let cont = 0; cont < 32; cont++) {
-                    entradasESaidasConcatenadas[i] = arrayArmazenaEmBool[n][[cont]]         //[1,0,1,1,0,0,1,0,1,0,1,0,0,0,0,0,1...
-                    i++
-                }
-            }
-        //})
+            return arrayArmazenaEmBool.reduce((total, indice) => total.concat(indice), [])
+         //})
     }
-
-    var preencheArraycomZeros = (array,quantidade) => {
-        for(let n = 0; n < quantidade; n++){
-            array.push(0)
-        }
-    }
-
 
     var todosIDs = []
     var _todosIDs = id => "#" + todosIDs[id]
@@ -67,27 +53,24 @@ setTimeout(() => {
 
     var adicionaClasseAosCartoes = (address,classe) => address.parent().removeClass().addClass(classe) 
 
-    var quantidadesDeIO = todosIDs.length      //127 ou 200
-
-    var entradasESaidasConcatenadas = []
-    preencheArraycomZeros(entradasESaidasConcatenadas,quantidadesDeIO)
-    var tempoEmON = []
-    preencheArraycomZeros(tempoEmON,quantidadesDeIO)
-
-    var SensCilGiraMesaAvancado
+    var entradasESaidasConcatenadas = Array(todosIDs.length).fill(0)//preencheArraycomZeros(todosIDs.length)
+    var tempoEmON = Array(todosIDs.length).fill(0)//preencheArraycomZeros(todosIDs.length)
 
     var backgroundcolor = (address,cor) => $(address).css('background-color',cor)
 
-    const somaMaisUmSeVariavelDentro = () => {
-        for(let n = 0; n < quantidadesDeIO; n++){
+    const somaMaisUmSeVariavelDentro = (arrayArmazenaTema) => {
+        for(let n = 0; n < todosIDs.length; n++){
+            console.log(arrayArmazenaTema);
             if(entradasESaidasConcatenadas[n]){
-                tempoEmON[n]++
+                arrayArmazenaTema[n]++
+                
             }  
         }
+        return arrayArmazenaTema
     }
 
     var pintaIdentificacaoRapida = () => {
-            for(let n = 0; n < quantidadesDeIO; n++){
+            for(let n = 0; n < todosIDs.length; n++){
                 entradasESaidasConcatenadas[n] ? backgroundcolor(_todosIDs(n),'#00ff00') : backgroundcolor(_todosIDs(n),'#dbdbdb')
                 mostraTempo($(_todosIDs(n)),n)
             }
@@ -112,19 +95,17 @@ setTimeout(() => {
     }
             //>>>>>$(".tamanho").css('height',parseInt(Math.random()*250)+'px')<<<<
 
-        $("#" + todosIDs[2]).parent().find(".tempoFinal").text("romel")
 
-
-    const resetaCiclo = () =>{
-        if((SensCilGiraMesaAvancado == 0) && (entradasESaidasConcatenadas[1] == 1)){ // ATUALIZA CONTADOR DE VARIAVEL DENTRO A CADA CICLO
-            for(let n = 0; n < quantidadesDeIO ; n++){
+    const resetaCiclo = (ArmazenaBorda) =>{
+        if((ArmazenaBorda == 0) && (entradasESaidasConcatenadas[1] == 1)){ // ATUALIZA CONTADOR DE VARIAVEL DENTRO A CADA CICLO
+            for(let n = 0; n < todosIDs.length ; n++){
                 if(moduloDeMonutoramento == "temposDeCiclo"){
-                    mostraTempo($(_todosIDs(n)).parent().find(".tempoFinal"),n)  // ATUALIZA CONTADOR EM TEMPOS DE CICLO QUANDO O CICLO ACABA
+                    mostraTempo($(_todosIDs(n)).parent().find(".tempoFinal"),n)  // ATUALIZA CONTADOR 
                 }
                 tempoEmON[n] = 0
             }
         }
-        SensCilGiraMesaAvancado = entradasESaidasConcatenadas[1]
+        ArmazenaBorda = entradasESaidasConcatenadas[1]
     }
 
     var identificaCartao = cart => {
@@ -149,7 +130,7 @@ setTimeout(() => {
 
     var mudaDePagDevice = () => {
         const addresToDevice = "http://10.2.65.150/awp/"
-        const AbreviaAjax = index => setInterval(() => ajaxValues(addresToDevice + index), 1100)
+        const AbreviaAjax = index => setInterval(() => entradasESaidasConcatenadas = ajaxValues(addresToDevice + index), 600)
         const replacePagDevice = (Device, cpuBool) => {
             window.location.replace(addresToDevice + Device + ".html")
             return cpuBool
@@ -168,25 +149,25 @@ setTimeout(() => {
         $(".IDEN").attr('id','IDENTIFICACAO_RAPIDA')
         moduloDeMonutoramento = "IDENTIFICACAO_RAPIDA"
         
-        for(let n = 0; n < quantidadesDeIO; n++){
+        for(let n = 0; n < todosIDs.length; n++){
             $(".tempoFinal").remove()
             $(_todosIDs(n)).text("")  
         }
 
+        let armazenaBorda = entradasESaidasConcatenadas[1]
         setInterval(() => {if(selecionaValor("#exibicao","IDENTIFICACAO_RAPIDA")){
-                somaMaisUmSeVariavelDentro()
-                pintaIdentificacaoRapida()
-            }
-        },10)
+            tempoEmON = somaMaisUmSeVariavelDentro(tempoEmON)
+            armazenaBorda = resetaCiclo(armazenaBorda)
+            pintaIdentificacaoRapida()
+        }},10)
 
-        setInterval(resetaCiclo);
     }
 
     const MainTemposDeCiclo = () => {
 
         $(".IDEN").attr('id','temposDeCiclo')
         moduloDeMonutoramento = "temposDeCiclo"
-        for(let n = 0; n < quantidadesDeIO; n++){
+        for(let n = 0; n < todosIDs.length; n++){
             $(_todosIDs(n)).parent().append("<div class='tempoFinal'>0.0s</div>")
             $(_todosIDs(n)).text("")
             backgroundcolor(_todosIDs(n),'#e2e2e2')
@@ -194,13 +175,13 @@ setTimeout(() => {
 
         var tamanhoCartao = identificaCartao($(".lupa:checked").val())
         $(".lupa").change(() => tamanhoCartao = trocaAsBarrasAoTrocarOCartao())
-        
+
+        let armazenaBorda = entradasESaidasConcatenadas[1]
         setInterval(() => {if(selecionaValor("#exibicao","temposDeCiclo")){
             preencheBarraTemposDeCiclo(tamanhoCartao)
-            somaMaisUmSeVariavelDentro();
+            tempoEmON = somaMaisUmSeVariavelDentro(tempoEmON)
+            armazenaBorda = resetaCiclo(armazenaBorda)
         }});
-
-        setInterval(resetaCiclo);
     }
 
     $("#exibicao").change(() => selecionaValor("#exibicao","IDENTIFICACAO_RAPIDA") ? MainIdentificacaoRapida() : MainTemposDeCiclo())
